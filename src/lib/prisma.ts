33d19d1b9ae73678d7a1f08f96f73db0b8ef73e0
Nottometto <imnotwenxing@gmail.com>
2026-07-14 02:03:29 +0800

    Connected better auth with prisma and prisma with neondb

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@/generated/prisma/client"; 
+import { PrismaNeon } from "@prisma/adapter-neon"; 
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL, 
+}); 
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export default prisma; 
